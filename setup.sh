3c5a55ebf3af7b9707b58170b357f6bb68f893de
Serge Bouchut <serge.bouchut@gmail.com>
2019-02-14 23:04:21 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -70,5 +70,18 @@ git clone git@github.com:SergeBouchut/patterns.git
 virtualenv ~/dev/.venv2 --python=python2
 virtualenv ~/dev/.venv3 --python=python3
 
-dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)use-custom-command true
-dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)custom-command "gnome-terminal -e 'tmux new-session -A -s main'"
+echo"
+visible-name='serge'
+login-shell=false
+palette=['rgb(7,54,66)', 'rgb(220,50,47)', 'rgb(133,153,0)', 'rgb(181,137,0)', 'rgb(38,139,210)', 'rgb(211,54,130)', 'rgb(42,161,152)', 'rgb(238,232,213)', 'rgb(0,43,54)', 'rgb(203,75,22)', 'rgb(88,110,117)', 'rgb(101,123,131)', 'rgb(131,148,150)', 'rgb(108,113,196)', 'rgb(147,161,161)', 'rgb(253,246,227)']
+custom-command="gnome-terminal -e 'tmux new-session -A -s main'"
+default-size-columns=95
+default-size-rows=50
+use-system-font=false
+use-custom-command=true
+use-transparent-background=true
+exit-action='close'
+font='Monospace 13'
+audible-bell=false
+background-transparency-percent=0
+" | dconf load /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)
