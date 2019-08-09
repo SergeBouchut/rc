f0aa371485998aa4fa604761d201023e09ff9380
Serge Bouchut <serge.bouchut@gmail.com>
2019-08-09 11:42:42 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo dnf install -y \
     https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
     https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-sudo dnf update
+sudo dnf update -y
 
 sudo dnf install -y \
     chrome-gnome-shell \
