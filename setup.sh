4bdeec4283de4b0cf4cb607c83e5a2dbe2c5f585
Serge Bouchut <serge.bouchut@gmail.com>
2019-08-09 12:15:53 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -44,6 +44,9 @@ wget https://raw.githubusercontent.com/SergeBouchut/rc/master/.gitconfig
 wget https://raw.githubusercontent.com/SergeBouchut/rc/master/.tmux.conf
 wget https://raw.githubusercontent.com/SergeBouchut/rc/master/.vimrc
 wget https://raw.githubusercontent.com/SergeBouchut/rc/master/.zshrc
+wget https://raw.githubusercontent.com/SergeBouchut/rc/master/gnome-terminal-profiles.dconf
+
+dconf load /org/gnome/terminal/legacy/profiles:/ < gnome-terminal-profiles.dconf
 
 ssh-keygen -t rsa -b 4096 -C "serge.bouchut@gmail.com"
 eval "$(ssh-agent -s)"
@@ -58,20 +61,3 @@ pip install \
    ipython \
    
    # pgcli \
-    
-# rename profile to create new one
-echo "
-visible-name='serge'
-login-shell=false
-palette=['rgb(7,54,66)', 'rgb(220,50,47)', 'rgb(133,153,0)', 'rgb(181,137,0)', 'rgb(38,139,210)', 'rgb(211,54,130)', 'rgb(42,161,152)', 'rgb(238,232,213)', 'rgb(0,43,54)', 'rgb(203,75,22)', 'rgb(88,110,117)', 'rgb(101,123,131)', 'rgb(131,148,150)', 'rgb(108,113,196)', 'rgb(147,161,161)', 'rgb(253,246,227)']
-custom-command="gnome-terminal -e 'tmux new-session -A -s main'"
-default-size-columns=95
-default-size-rows=50
-use-system-font=false
-use-custom-command=true
-use-transparent-background=true
-exit-action='close'
-font='Monospace 13'
-audible-bell=false
-background-transparency-percent=0
-" | dconf load /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)
