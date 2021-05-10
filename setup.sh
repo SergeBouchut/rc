be157b466e2f558682f41c121047144e5ac38f1c
Serge Bouchut <serge.bouchut@gmail.com>
2021-05-10 22:10:22 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/sh
 
-sudo dnf install -y \
+sudo dnf -y install \
     https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
     https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-sudo dnf update -y
+sudo dnf -y update
 
-sudo dnf install -y \
+sudo dnf -y install \
     gnome-tweaks \
     htop \
     httpie \
@@ -20,66 +20,32 @@ sudo dnf install -y \
     xclip \
     zsh \
 
-    # chrome-gnome-shell \
-    # libpq-devel \
-    # postgresql \
-    # python3-virtualenv \
-    # qgis \    
-    # spatialindex \
-    # spatialindex-devel \
-
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io
+sudo dnf -y install docker-ce docker-ce-cli containerd.io
 
-# f32 issues with cgroup v2
-# sudo dnf install --releasever=31 docker-ce docker-ce-cli containerd.io
-# sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"  # reboot
-# sudo vim /etc/firewalld/firewalld.conf  # FirewallBackend=iptables
-# sudo systemctl restart firewalld.service
-
-sudo usermod -aG docker $USER # logout or wait for reboot below
+sudo usermod -aG docker $USER
 sudo systemctl enable docker --now
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 flatpak install -y \
     com.slack.Slack \
-    org.signal.Signal \
     org.telegram.desktop \
 
-    # com.spotify.Client \
-    # com.valvesoftware.Steam \
-    # com.visualstudio.code \
-    # org.octave.Octave \
-    
-    # io.atom.Atom \
-    # net.minetest.Minetest \
-
-chsh -s $(which zsh) # relogin
+chsh -s $(which zsh)
 
 wget https://raw.githubusercontent.com/SergeBouchut/rc/master/.gitconfig
 wget https://raw.githubusercontent.com/SergeBouchut/rc/master/.tmux.conf
 wget https://raw.githubusercontent.com/SergeBouchut/rc/master/.vimrc
 wget https://raw.githubusercontent.com/SergeBouchut/rc/master/.zshrc
-wget https://raw.githubusercontent.com/SergeBouchut/rc/master/gnome-terminal-profiles.dconf
 
-dconf load /org/gnome/terminal/legacy/profiles:/ < gnome-terminal-profiles.dconf
+# wget https://raw.githubusercontent.com/SergeBouchut/rc/master/gnome-terminal-profiles.dconf
+# dconf load /org/gnome/terminal/legacy/profiles:/ < gnome-terminal-profiles.dconf
 
-gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']" # reboot
 # todo enable compose key right ctrl
-# https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html/desktop_migration_and_administration_guide/enable-compose-key
 
 ssh-keygen -t rsa -b 4096 -C "serge.bouchut@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 xclip -sel clip < ~/.ssh/id_rsa.pub
-
-mkdir ~/dev
-python -m venv ~/dev/.venv
-source ~/dev/.venv/bin/activate
-
-pip install \
-   ipython \
-   
-   # pgcli \
