93dfc6001ec7a7d2cdb0d4d8bbd9cf195f400538
Serge Bouchut <serge.bouchut@gmail.com>
2021-07-10 21:14:50 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -48,7 +48,7 @@ rm gnome-terminal-profiles.dconf
 
 gnome-tweaks  # keyboard > compose key > enable > right ctrl
 
-ssh-keygen -t rsa -b 4096 -C "serge.bouchut@gmail.com"
+ssh-keygen -t ed25519 -C "serge.bouchut@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 xclip -sel clip < ~/.ssh/id_rsa.pub
