d3ab2eee5a5dc76642946f595686de6c73974768
Serge Bouchut <serge.bouchut@gmail.com>
2021-07-10 14:38:26 +0200

    Update docker compose version

--- setup.sh
+++ setup.sh
@@ -35,7 +35,7 @@ sudo dnf -y install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
 sudo systemctl enable docker --now
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 wget https://raw.githubusercontent.com/SergeBouchut/rc/master/.gitconfig
