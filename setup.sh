0797ca732c5863c58a7556e07d8896b26f9fd5af
Serge Bouchut <serge.bouchut@gmail.com>
2023-03-02 17:05:43 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,7 +27,7 @@ flatpak install -y \
 
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker $USER
 sudo systemctl enable docker --now
 
